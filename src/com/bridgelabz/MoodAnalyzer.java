e951b1c3dcf818e54be4c2d1256c4a2d4cae6816
saps999 <saptarshibiswasgang@gmail.com>
2022-09-27 22:27:32 +0530

    Call analyseMood function with message as parameter and return Happy or Sad Mood

--- src/com/bridgelabz/MoodAnalyzer.java
+++ src/com/bridgelabz/MoodAnalyzer.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-    public static void main(String[] args) {
+    public String analyseMood(String message) {
+        if (message.contains("sad")) {
+            System.out.println("SAD");
+            return "SAD MOOD";
+        } else {
+            System.out.println("HAPPY");
+            return "HAPPY MOOD";
+        }
+    }
 
+    public static void main(String[] args) {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+        moodAnalyser.analyseMood("I'm in happy mood today");
     }
 }
