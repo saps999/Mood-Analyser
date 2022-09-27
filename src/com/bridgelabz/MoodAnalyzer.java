73aac39018e4ad3256ffc434685298f104a1a82a
saps999 <saptarshibiswasgang@gmail.com>
2022-09-27 23:18:02 +0530

    Handling NULL Scenario using try catch and return Happy

--- src/com/bridgelabz/MoodAnalyzer.java
+++ src/com/bridgelabz/MoodAnalyzer.java
@@ -2,26 +2,28 @@ package com.bridgelabz;
 
 public class MoodAnalyzer {
     String message;
+
     public MoodAnalyzer(String message) {
-        this.message=message;
+        this.message = message;
     }
 
     public static String analyseMood(String message) {
         try {
-            message.contains("sad");
+            message.contains(null);
+            System.out.println("HAPPY");
+            return "HAPPY MOOD";
+
+        } catch (NullPointerException e) {
             System.out.println("SAD");
             return "SAD MOOD";
         }
-        catch (NullPointerException e){
-            System.out.println("HAPPY");
-            return "HAPPY MOOD";
-        }
     }
-    public void analyse(){
+
+    public void analyse() {
         MoodAnalyzer.analyseMood(message);
     }
 
     public static void main(String[] args) {
-       new MoodAnalyzer("I am in sad mood").analyse();
+        new MoodAnalyzer("").analyse();
     }
 }
